4e6e9dce73e1d72ac1fc3c3e458cee620fc9af01
mdali8630 <mohammadali10298@gmail.com>
2025-09-14 10:42:42 +0530

    upload-code

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from 'src/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/core/entities/user.entity';
 import { VirtualAccount } from 'src/core/entities/virtual-account.entity';
+import { Transaction } from 'src/core/entities/transactions.entity';
 import { Wallet} from 'src/core/entities/wallet.entity';
 import { IntegrationModule } from 'src/integration/integration.module';
 import { ConfigModule } from '@nestjs/config';
@@ -27,7 +28,7 @@ import { NotificationBridge } from 'src/notifications/services/notification-brid
 @Module({
   imports: [AuthModule,
     BullModule.registerQueue({name: 'notification'}),
-     HttpModule, IntegrationModule, CardsModule, WalletModule, ConfigModule, forwardRef(() => WalletModule), TypeOrmModule.forFeature([User,VirtualAccount,Wallet, UserDocument, OtpInfo, AadharResponse])],
+     HttpModule, IntegrationModule, CardsModule, WalletModule, ConfigModule, forwardRef(() => WalletModule), TypeOrmModule.forFeature([User,Transaction,VirtualAccount,Wallet, UserDocument, OtpInfo, AadharResponse])],
   providers: [UsersService, ConfigService, NotificationBridge, UploadFileService, OtpFlowService, SmsClientService, MailService, OtpRepository],
   controllers: [UsersController],
   exports: [UsersService, UploadFileService],
